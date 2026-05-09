88c50aa97ef0de57cd29e8fd82e23a455cb17133
iPeer <chrisdwignall+github@gmail.com>
2026-05-10 00:13:05 +0100

    Feat: Add Spansh (csv) parsing

---
+++ Akiyama.EDOCCommon/Spansh/PlottedRoute.cs
@@ -0,0 +1,44 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace Akiyama.EDOCCommon.Spansh
+{
+    public class PlottedRoute
+    {
+
+
+        public List<RouteEntry> Jumps = [];
+
+        public static PlottedRoute FromFile(string path)
+        {
+            PlottedRoute route = new();
+
+            // Determine whether we're dealing with a JSON file, or a CSV file
+            // Do we trust the extension or not? (Would a user deliberately change it to fuck with us? Well, if they do, they'll crash their shit so that's on them I guess
+            if (Path.GetExtension(path) == ".csv")
+            {
+                CsvConfiguration cfg = CsvConfiguration.FromAttributes<RouteEntry>();
+                using (StreamReader r = new StreamReader(path))
+                {
+                    using (CsvReader csv = new CsvReader(r, cfg))
+                    {
+                        route.Jumps = csv.GetRecords<RouteEntry>().ToList();
+                    }
+                }
+            }
+            else if (Path.GetExtension(path) == ".json")
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("The file provided was not a valid Spansh route export", "Incorrect file type");
+            }
+
+
+            return route;
+        }
+
+
+    }
+}
---
+++ Akiyama.EDOCCommon/Spansh/RouteEntry.cs
@@ -0,0 +1,43 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System.Text.Json.Serialization;
+
+namespace Akiyama.EDOCCommon.Spansh
+{
+    [CultureInfo("en-GB")]
+    public class RouteEntry
+    {
+
+        [Name("System Name")]
+        [JsonPropertyName("name")]
+        public string SystemName { get; set; }
+
+        [Name("Distance")]
+        [JsonPropertyName("distance")]
+        public decimal Distance { get; set; }
+
+        [Name("Distance Remaining")]
+        [JsonPropertyName("distance_to_destination")]
+        public decimal DistanceRemaining { get; set; }
+
+        [Name("Fuel Left")]
+        [JsonPropertyName("fuel_in_tank")]
+        public decimal FuelLeft { get; set; }
+
+        [Name("Fuel Used")]
+        [JsonPropertyName("fuel_used")]
+        public decimal FuelUsed { get; set; }
+
+        [Name("Refuel")]
+        [BooleanTrueValues(["Yes", "yes", "1"])]
+        [BooleanFalseValues(["No", "no", "0"])]
+        [JsonPropertyName("must_refuel")]
+        public bool Refuel { get; set; }
+
+        [Name("Neutron Star")]
+        [BooleanTrueValues(["Yes", "yes", "1"])]
+        [BooleanFalseValues(["No", "no", "0"])]
+        [JsonPropertyName("has_neutron")]
+        public bool Neutron { get; set; }
+
+    }
+}
